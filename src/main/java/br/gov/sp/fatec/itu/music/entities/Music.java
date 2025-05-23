2978fa0da582b587e67155aacd96192e075d63fe
Nicolas Ferreira <nicolas.as.ferreira@hotmail.com>
2025-05-23 12:25:29 -0300

    Criando entidade Music

---
+++ src/main/java/br/gov/sp/fatec/itu/music/entities/Music.java
@@ -0,0 +1,84 @@
+package br.gov.sp.fatec.itu.music.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "TBL_MUSICS")
+public class Music implements Serializable{
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String nome;
+    private String cantor;
+    private String album;
+    private String genero;
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCantor() {
+        return cantor;
+    }
+    public void setCantor(String cantor) {
+        this.cantor = cantor;
+    }
+    public String getAlbum() {
+        return album;
+    }
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Music other = (Music) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+
+    
+    
+}
