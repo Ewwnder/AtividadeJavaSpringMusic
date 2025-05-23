962134ee1b6e3d3c2c2e43a4176b2b40c02df394
Nicolas Ferreira <nicolas.as.ferreira@hotmail.com>
2025-05-23 12:41:33 -0300

    Criando data.sql e verificando dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_MUSICS (NOME, CANTOR, ALBUM, GENERO) VALUES ('Maniac', 'Michael Sembello', 'Flashdance', 'Dance-pop');
+INSERT INTO TBL_MUSICS (NOME, CANTOR, ALBUM, GENERO) VALUES ('Yes, And?', 'Ariana Grande', 'Eternal Sunshine', 'Pop');
+INSERT INTO TBL_MUSICS (NOME, CANTOR, ALBUM, GENERO) VALUES ('Chop Suey!', 'System of a Down', 'Toxicity', 'Rock');
