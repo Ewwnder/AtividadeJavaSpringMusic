2de0598e505b4f0580c1cec835d91d64ab5b7f4b
Ewwnder <nicolas.as.ferreira@hotmail.com>
2025-06-02 19:52:18 -0300

    Delete and Update

--- src/main/java/br/gov/sp/fatec/itu/music/controllers/MusicController.java
+++ src/main/java/br/gov/sp/fatec/itu/music/controllers/MusicController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +32,17 @@ public class MusicController {
     public ResponseEntity<Music> save(@RequestBody Music music){
         return ResponseEntity.created(null).body(service.save(music));
     }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void>delete(@PathVariable long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> update(@PathVariable long id, @RequestBody Music music){
+        service.update(music, id);
+        return ResponseEntity.noContent().build();
+    }
 }
+    
--- src/main/java/br/gov/sp/fatec/itu/music/services/MusicService.java
+++ src/main/java/br/gov/sp/fatec/itu/music/services/MusicService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.itu.music.entities.Music;
 import br.gov.sp.fatec.itu.music.repositories.MusicRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service   
 public class MusicService {
@@ -20,6 +21,25 @@ public class MusicService {
 
     public Music save(Music music){
         return repository.save(music);
+    }
+
+    public void update(Music music, long id){
+        Music aux = repository.getReferenceById(id);
+        aux.setAlbum(music.getAlbum());
+        aux.setCantor(music.getCantor());
+        aux.setGenero(music.getGenero());
+        aux.setNome(music.getNome());
+
+        repository.save(aux);
+    }
+
+    public void delete(long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        }
+        else{
+            throw new EntityNotFoundException("Aluno não cadastrado!");
+        }
     }
     
 }
